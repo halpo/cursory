8e566ade6052557192eba56898f98ec988f2f508
Andrew Redd <Andrew.Redd@hsc.utah.edu>
2019-07-18 14:39:03 -0600

    Improved cursory functions. Extended Testing.

--- R/cursory.R
+++ R/cursory.R
@@ -1,10 +1,14 @@
-#' @import pkgcond
 #' @import dplyr
+#' @import purrr
+#' @importFrom tidyselect everything
+#' @importFrom rlang :=
 NULL
+
+# Generics --------------------------------------------------------
 
 #' Cursory Functions
 #'
-#' look Cursory functions act like the dplyr `summarise_(all|at|if)`
+#' Cursory functions act like the dplyr `summarise_(all|at|if)`
 #' functions with an important difference, they put the variable name
 #' in a column and for each function passed in it puts the value in
 #' it's own column.
@@ -14,78 +18,100 @@ NULL
 #' * `cursory_if()` is the analog of [dplyr::summarise_if()]
 #'
 #' @inheritParams dplyr::summarise_all
+#' @param var.name Name of the column with variable names.
 #' @export
 cursory_all <- function (.tbl, .funs, ..., var.name="Variable") UseMethod("cursory_all")
 
-#' @describeIn cursory_all
+#' @rdname cursory_all
 #' @export
 cursory_at <- function (.tbl, .vars, .funs, ..., var.name="Variable") UseMethod("cursory_at")
 
-#' @describeIn cursory_all
+#' @rdname cursory_all
 #' @export
 cursory_if <- function (.tbl, .predicate, .funs, ..., var.name="Variable") UseMethod("cursory_if")
 
-cursory_all.tbl_lazy <-
-function (.tbl, .funs, ..., var.name="Variable")
+
+# Methods ---------------------------------------------------------
+
+#' @export
+cursory_at.tbl <-
+function (.tbl, .vars, .funs, ...)
 {
-    assert_that(.tbl %inherits% "tbl_lazy", is.function(.funs) ||
-        .funs %inherits% "fun_list")
-    vars <- setdiff(tbl_vars(.tbl), group_vars(.tbl))
-    funs <- dplyr:::as_fun_list(.funs, enquo(.funs), ...)
-    cenv <- rlang::caller_env()
-    do1 <- function(fun, name, ...) {
-        s1 <- dplyr:::manip_all(.tbl, fun, fun, cenv, ...)
-        dplyr::summarise(.tbl, !!!s1) %>% dplyr::group_by(!!!dplyr::groups(.tbl)) %>%
-            pivot::unpivot(Variable, !!name, everything())
-    }
-    purrr::reduce( purrr::imap(funs, do1, ...)
-                 , dplyr::left_join
-                 , by = c(group_vars(.tbl), "Variable"))
+    parts <- purrr::map( tidyselect::vars_select(tbl_vars(.tbl), !!!.vars)
+              , cursory_1, .tbl=.tbl, .funs=.funs
+              , ...)
+    purrr::reduce(parts, union_all)
 }
 if(FALSE){#@testing
+    library(dbplyr)
+    library(RSQLite)
+    con <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
 
-
+    suppress_warnings( .tbl <- group_by(copy_to(con, iris, 'iris', overwrite=TRUE), Species)
+                     , "partial argument match")
+    .vars <- setdiff(tbl_vars(.tbl), group_vars(.tbl))
+    .funs <- lst(mean, sum)
+    val <- cursory_at( .tbl, .vars, .funs)
 }
 
+cursory_1 <- function(var, .tbl, .funs, ..., var.name="Variable")
+{
+    select( mutate( summarise_at(.tbl, var, .funs, ...)
+                  , !!var.name := var)
+          , !!var.name, everything()
+          )
+}
 
-
+#' @export
 cursory_at.tbl_df <-
 function (.tbl, .vars, .funs, ...)
 {
-    .l <- purrr::imap(.funs, function(fun, name) tidyr::gather(summarise_at(.tbl,
-        .vars, fun, ...), Variable, !!name))
-    purrr::reduce(.l, full_join, by = c(group_vars(.tbl), "Variable"))
+    purrr::map_dfr( tidyselect::vars_select(tbl_vars(.tbl), !!!.vars)
+                  , cursory_1, .tbl=.tbl, .funs=.funs
+                  , ...)
 }
 
-cursory_at.tbl_lazy <-
-function (.tbl, .vars, .funs, ...)
+#' @export
+cursory_at.grouped_df <-
+function(.tbl, .vars, .funs, ...)
 {
-    vars <- select_vars(tbl_vars(.tbl), !!!.vars, exclude = group_vars(.tbl))
-    info.df <- expand.grid(Variable = vars, ._METRIC_. = names(.funs)) %>%
-        mutate(._X_. = paste(Variable, ._METRIC_., sep = "_")) %>%
-        copy_to(.tbl$src, df = ., name = paste("##CURSORY_INFO_DF",
-            digest::digest(.), digest::digest(.tbl, "crc32"),
-            digest::digest(.funs, "crc32"), sep = "_"),
-            overwrite = TRUE)
-    summarise_at(.tbl, .vars, .funs, ...) %>% group_by(!!!groups(.tbl)) %>%
-        pivot::unpivot(._X_., ._VALUE_., everything()) %>% dplyr::full_join(info.df,
-        by = "._X_.") %>% select(-._X_.) %>% pivot::pivot(._METRIC_.,
-        ._VALUE_., !!!(as_quos(names(.funs))))
+    group_by( NextMethod("cursory_at", .tbl, .vars=.vars, .funs=.funs, ...)
+            , !!!groups(.tbl)
+            )
 }
 
-cursory_if.tbl_lazy <-
-function (.tbl, .predicate, .funs, ...)
+#' @export
+cursory_at.data.frame <- function(.tbl, ...) cursory_at(tbl_df(.tbl), ...)
+
+
+#' @export
+cursory_all.tbl <-
+function(.tbl, .funs, ...)
 {
-    vars <- tbl_vars(select_if(ungroup(.tbl), .predicate))
-    info.df <- expand.grid(Variable = vars, ._METRIC_. = names(.funs)) %>%
-        dplyr::mutate(._X_. = paste(Variable, ._METRIC_., sep = "_")) %>%
-        dplyr::copy_to(.tbl$src, df = ., name = paste("##CURSORY_INFO_DF",
-            digest::digest(.), digest::digest(.tbl, "crc32"),
-            digest::digest(.funs, "crc32"), sep = "_"),
-            overwrite = TRUE)
-    summarise_if(.tbl, .predicate, .funs, ...) %>% group_by(!!!groups(.tbl)) %>%
-        pivot::unpivot(._X_., ._VALUE_., everything(), -one_of(group_vars(.tbl))) %>%
-        dplyr::full_join(info.df, by = "._X_.") %>% select(-._X_.) %>%
-        pivot::pivot(._METRIC_., ._VALUE_., !!!(as_quos(names(.funs))))
+    cursory_at( .tbl = .tbl
+              , .vars = setdiff(tbl_vars(.tbl), group_vars(.tbl))
+              , .funs = .funs
+              , ...)
+}
+if(FALSE){#@testing
+    .tbl <- group_by(as_tibble(iris), Species)
+    .funs <- lst(Missing = . %>% is.na %>% sum(na.rm = TRUE)
+                , mean, sd )
+
+    val <- cursory_all(.tbl, .funs)
+    expect_is(val, 'tbl_df')
+    expect_identical( as.character(tbl_vars(val))
+                    , c('Variable', 'Species'
+                       , 'Missing', 'mean', 'sd'))
+    expect_identical(dim(val), c(12L, 5L))
+
+    expect_is(val, 'grouped_df')
+    expect_equal(group_vars(val), 'Species')
 }
 
+#' @export
+cursory_if.data.frame <-
+function(.tbl, .predicate, .funs, ...)
+{
+    cursory_all(select_if(.tbl, .predicate), .funs, ...)
+}
