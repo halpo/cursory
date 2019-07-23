294470fe603720cde63a090bcaa55638935bf6d5
Andrew Redd <Andrew.Redd@hsc.utah.edu>
2019-07-23 12:14:08 -0600

    Npct refinement and testing.

--- R/Npct.R
+++ R/Npct.R
@@ -4,26 +4,64 @@
 #' @param n see methods.
 #' @param ... formatting arguments for formatting the percent. See format.percent.
 #' @export
-Npct <- function(x, n, ...){stop("Not implimented")}
+Npct <- function(x, n, ...){pkg_error("Not implimented")}
 setGeneric('Npct')
+if(FALSE){#@testing
+    expect_error(Npct("not", "valid"))
+}
+
+
 
 #' @describeIn Npct Count and give percent of `TRUE` from a logical vector.
 setMethod('Npct', signature('logical', 'missing'), function(x, n, ...){
-    sprintf('%d (%s)', sum(x), format.percent(mean(x), ...))
+    count <- sum(x)
+    percent <- mean(x)
+    structure( sprintf('%d (%s)', count, pct(percent, ...))
+             , count = count, percent=percent)
 })
+if(FALSE){#@testing
+    val <- Npct(c(T, F, T))
+    expect_equal(as.character(val), '2 (66.67%)')
+    expect_identical(attr(val, 'count'), 2L)
+    expect_identical(attr(val, 'percent'), 2/3)
+}
+
 #' @describeIn Npct Count and percent of a logical filtered by a second logical.
 setMethod('Npct', signature('logical', 'logical'), function(x, n, ...){
     assert_that(length(x) == length(n))
-    sprintf('%d (%s)', sum(x[n]), format.percent(mean(x[n]), ...))
+    count <- sum(x[n])
+    percent <- mean(x[n])
+    structure( sprintf('%d (%s)', count, pct(percent, ...))
+             , count = count, percent=percent)
 })
+if(FALSE){#@testing
+    val <- Npct( c(T,F,T,F,T)
+               , c(T,T,T,F,F)
+               )
+    expect_equal(as.character(val), '2 (66.67%)')
+    expect_identical(attr(val, 'count'), 2L)
+    expect_identical(attr(val, 'percent'), 2/3)
+}
+
 #' @describeIn Npct Provided with count(s) of cases and total(s)
 setMethod('Npct', signature('integer', 'integer'), function(x, n, ...){
     assert_that( all(x <  n)
                , all(x >= 0)
                , all(n >  0)
     )
-    sprintf("%d (%s)", x, format.percent(x/n, ...))
+    count <- x
+    percent <- x/n
+    structure( sprintf('%d (%s)', count, pct(percent, ...))
+             , count = count, percent=percent)
 })
+if(FALSE){#@testing
+    val <- Npct(2L, 3L)
+    expect_equal(as.character(val), '2 (66.67%)')
+    expect_identical(attr(val, 'count'), 2L)
+    expect_identical(attr(val, 'percent'), 2/3)
+}
+
+
 #' @describeIn Npct Provided the actual count and the percent.
 setMethod('Npct', signature('numeric', 'numeric'), function(x, n, ...){
     assert_that( rlang::is_integerish(x)
@@ -31,10 +69,14 @@ setMethod('Npct', signature('numeric', 'numeric'), function(x, n, ...){
                , all(0 <= n & n <= 1)
                , length(x) == length(n)
                )
-    sprintf("%d (%s)", x, format.percent(n, ...))
+    count <- as.integer(x)
+    percent <- n
+    structure( sprintf('%d (%s)', count, pct(percent, ...))
+             , count = count, percent=percent)
 })
-
-Npct_sql <- function(n, pct){
-    assert_that(requireNamespace("dbplyr"))
-    dbplyr::build_sql("FORMAT(", n, ", 'D') + '(' + ", "FORMAT(", pct, ",'P') + ')'")
+if(FALSE){#@testing
+    val <- Npct( 2, 2/3)
+    expect_equal(as.character(val), '2 (66.67%)')
+    expect_identical(attr(val, 'count'), 2L)
+    expect_identical(attr(val, 'percent'), 2/3)
 }
